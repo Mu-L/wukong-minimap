0ff5b4f9e930e055a411a11514c8a643782be160
jaskang <jaskang@qq.com>
2025-02-10 12:58:00 +0800

    延迟执行

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,16 +1,21 @@
 #include <windows.h>
 #include "draw.h"
 
+#include <thread>
+#include <chrono>
 
-void startup_thread(HMODULE poc_module) {
+void startup_thread(HMODULE poc_module)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(20));
 	DrawInit();
 }
 
-
-BOOL WINAPI DllMain(HMODULE handle, DWORD reason, LPVOID reserved) {
+BOOL WINAPI DllMain(HMODULE handle, DWORD reason, LPVOID reserved)
+{
 	DisableThreadLibraryCalls(handle);
-	if (reason == DLL_PROCESS_ATTACH) {
-			CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)startup_thread, handle, 0, nullptr);
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)startup_thread, handle, 0, nullptr);
 	}
 
 	return TRUE;
